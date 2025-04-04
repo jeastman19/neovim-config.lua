0ce3bca9f409697f9c790e9adea4e3fa00b3182b
Miguel Hernandez Giusti <mh@andsoluciones.com>
2025-04-04 14:01:42 -0500

    Add fzf-lua plugin

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup({
---      api_key_cmd = "op read op://Employee/chatgptkey/credential --no-newline", -- OnePassword example
+      api_key_cmd = "op read op://Employee/chatgptkey/credential --no-newline", -- OnePassword example
       openai_params = {
         -- NOTE: model can be a function returning the model name
         -- this is useful if you want to change the model on the fly
@@ -30,7 +30,7 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
-    "folke/trouble.nvim",
-    "nvim-telescope/telescope.nvim"
+    "folke/trouble.nvim"
+    -- "ibhagwan/fzf-lua"
   }
 }
